1843cdea8d1db72b042245e7962d20beb1b94ded
geethaalampally <alampallygeetha62@gmail.com>
2026-06-23 23:21:16 +0530

    Time: 438 ms (12.30%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -0,0 +1,9 @@
+SELECT actor_id, director_id
+FROM (
+    SELECT actor_id,
+           director_id,
+           COUNT(*) AS score
+    FROM ActorDirector
+    GROUP BY actor_id, director_id
+    HAVING COUNT(*) >= 3
+) temp;
