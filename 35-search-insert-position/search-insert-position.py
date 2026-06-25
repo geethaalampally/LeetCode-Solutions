33db8eeb7f0c09819d1942b030fc1ec150507e27
geethaalampally <alampallygeetha62@gmail.com>
2026-06-25 22:52:56 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (11.03%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -2,20 +2,25 @@ class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         left = 0
         right = len(nums) - 1
-
+        ans=len(nums)
         while left <= right:
             mid = (left + right) // 2
 
-            if nums[mid] == target:
-                return mid
+            # if nums[mid] == target:
+            #     return mid
 
-            elif nums[mid] < target:
-                left = mid + 1
+            # elif nums[mid] < target:
+            #     left = mid + 1
 
+            # else:
+            #     right = mid - 1
+            if nums[mid]>=target:
+                ans=mid
+                right=mid-1
             else:
-                right = mid - 1
+                left=mid+1
 
-        return left 
+        return ans 
 
         # same concept in longest increasing sub sequence 
         # where the tail value is updated 
