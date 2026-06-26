b4238aa1e0ec0be9d37a2a38e35811aaf4f6df76
geethaalampally <alampallygeetha62@gmail.com>
2026-06-26 12:29:14 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (62.02%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,13 +1,15 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        val1=0
-        val2=0
-        for char in s:
-            val1+=ord(char)
-        for char in t:
-            val2+=ord(char)
-        diff=val2-val1
-        return chr(diff)
+        # val1=0
+        # val2=0
+        # for char in s:
+        #     val1+=ord(char)
+        # for char in t:
+        #     val2+=ord(char)
+        # diff=val2-val1
+        # return chr(diff)
 
-        
+        for char in t:
+            if s.count(char)!=t.count(char):
+                return char
         
