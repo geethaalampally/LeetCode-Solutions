c80c17e2d225fdbec03e9d82e04dde087496c4b6
geethaalampally <alampallygeetha62@gmail.com>
2026-07-02 16:21:10 +0530

    Time: 3 ms (61.35%) | Memory: 21 MB (93.14%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,11 +1,15 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        seen=set()
-        for num in nums:
-            if num not in seen:
-                seen.add(num)
-            else:
-                seen.remove(num)
-        res=list(seen)
-        return res[0]
+        # seen=set()
+        # for num in nums:
+        #     if num not in seen:
+        #         seen.add(num)
+        #     else:
+        #         seen.remove(num)
+        # res=list(seen)
+        # return res[0]
         
+        xor=0
+        for num in nums:
+            xor=xor^num
+        return xor
